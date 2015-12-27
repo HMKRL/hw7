0a36e2ca0e85ac46dc32e1b71b92816c1ea69d49
Yu-Cheng Liang <a135246a135246@gmail.com>
2015-12-27 19:08:28 +0800

    use link list

--- hw7.c
+++ hw7.c
@@ -1,22 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int bucket_union(int **orig_bucket, FILE *infp);
-int new_mapping();
+struct mapping {
+	int value;
+	struct mapping *next;
+};
+
+struct m_index {
+	struct mapping *map;
+	struct m_index *next;
+};
+
+typedef struct mapping Map;
+typedef struct m_index Mi;
+
+int bucket_union(int**, FILE*);
+int new_mapping(Mi*, int**, const int);
 void print_result();
 
 int main() {
 	int i, j, rows = 0, n;
-	char input_file[11] = "buckets.in\0", *row = malloc(200 * sizeof(char));
+	char *row = malloc(200 * sizeof(char));
 	FILE *infp;
+	Mi *firstMi = NULL;
 
 	printf("Please input n:");
 	scanf("%d",&n);
 
 //open input file, if file not exist, show error message.
-	infp = fopen(input_file, "r");
+	infp = fopen("buckets.in", "r");
 	if(infp == NULL) {
-		printf("ERROR:Input file not found.");
+		fprintf(stderr, "%s\n", "ERROR:Input file not found.");
 		exit(1);
 	}
 
@@ -25,6 +39,7 @@ int main() {
 		rows++;
 	}
 	free(row);
+
 	int **orig_bucket;
 	orig_bucket = malloc(rows * sizeof(int*));
 	for(i = 0;i < rows;i++) {
@@ -39,6 +54,7 @@ int main() {
 		rewind(infp);
 	}
 
+
 	for(i = 0;i < rows;i++) {
 		for(j = 0;j < 40;j++) {
 			if(orig_bucket[i][j] != 0) {
@@ -48,7 +64,7 @@ int main() {
 		printf("\n");
 	}
 
-//release allocated memory
+//free allocated memory
 	for(i = 0;i < rows;i++) {
 		free(orig_bucket[i]);
 	}
@@ -62,36 +78,53 @@ int bucket_union(int **orig_bucket, FILE *infp) {
 	char temp;
 	while(fscanf(infp, "%d%c", &orig_bucket[curr_row][curr_num], &temp) != EOF) {
 		curr_num++;
-		if(temp == '\r') {
+		if(temp == '\r' || temp == '\n') {
 			curr_row++;
 			curr_num = 0;
 		}
 	}
-/*
-	while(1) {
-		temp = fgetc(infp);
-	
-		if(temp == EOF) {
-			number = 0;
-			break;
-		}
-	
-		if(temp == ' ') {
-			orig_bucket[curr_row][curr_num] = number;
-			curr_num++;
-			number = 0;
-		}
-		else if(temp == '\n') {
-			orig_bucket[curr_row][curr_num] = number;
-			curr_row++;
-			curr_num = 0;
-			number = 0;
-		}
-		else if((int)temp > 47 && (int)temp < 58) {
-			number *= 10;
-			number += (int)temp - 48;
-		}
-	}
-*/
+
 	return ++curr_row;
+}
+
+int new_mapping(Mi *firstMi, int **orig_bucket, const int n) {
+	int curr_row = 0, curr_num = 0, count = 0;
+	Mi *curMi = firstMi;
+	Map *curMap;
+	while(orig_bucket[curr_row][0] != 0) {
+		if(curMi != NULL) {
+			curr_num = 0;
+			count = 0;
+			curMap = curMi -> map;
+			while(orig_bucket[curr_row][curr_num] != 0 || curMap != NULL) {
+				if(orig_bucket[curr_row][curr_num] != curMap -> value) {
+					if(orig_bucket[curr_row][curr_num] > curMap -> value) {
+						count++;
+						curMap = curMap -> next;
+					}
+					else {
+						count++;
+						curr_num++;
+					}
+				}
+				else {
+					count++;
+					curMap = curMap -> next;
+					curr_num++;
+				}
+			}
+			while(orig_bucket[curr_row][curr_num] != 0) {
+				count++;
+				curr_num++;
+			}
+			if((count) <= n) {
+//save current row into new mapping
+				curr_row++;
+			}
+			else {
+//scan next bucket
+				curMi = curMi -> next;
+			}
+		}
+	}
 }
