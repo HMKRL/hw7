fe8da2cc61b958b06d5cbb3def49854b5778d55d
Yu-Cheng Liang <a135246a135246@gmail.com>
2015-12-27 14:33:07 +0800

    new way to read number

--- hw7.c
+++ hw7.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int bucket_union();
+int bucket_union(int **orig_bucket, FILE *infp);
 int new_mapping();
 void print_result();
 
 int main() {
-	int i, j, rows = 0, curr_row = 0, curr_num = 0, n, number = 0;
-	char temp, input_file[11] = "buckets.in\0", *row = malloc(200 * sizeof(char));
+	int i, j, rows = 0, n;
+	char input_file[11] = "buckets.in\0", *row = malloc(200 * sizeof(char));
 	FILE *infp;
 
 	printf("Please input n:");
@@ -35,6 +35,39 @@ int main() {
 	rewind(infp);
 
 //get number from file"buckets.in" and store into array
+	while(bucket_union(orig_bucket, infp) != rows) {
+		rewind(infp);
+	}
+
+	for(i = 0;i < rows;i++) {
+		for(j = 0;j < 40;j++) {
+			if(orig_bucket[i][j] != 0) {
+				printf("%d ",orig_bucket[i][j]);
+			}
+		}
+		printf("\n");
+	}
+
+//release allocated memory
+	for(i = 0;i < rows;i++) {
+		free(orig_bucket[i]);
+	}
+	free(orig_bucket);
+
+	return 0;
+}
+
+int bucket_union(int **orig_bucket, FILE *infp) {
+	int curr_row = 0, curr_num = 0;
+	char temp;
+	while(fscanf(infp, "%d%c", &orig_bucket[curr_row][curr_num], &temp) != EOF) {
+		curr_num++;
+		if(temp == '\r') {
+			curr_row++;
+			curr_num = 0;
+		}
+	}
+/*
 	while(1) {
 		temp = fgetc(infp);
 	
@@ -59,15 +92,6 @@ int main() {
 			number += (int)temp - 48;
 		}
 	}
-//release allocated memory
-	for(i = 0;i < rows;i++) {
-		free(orig_bucket[i]);
-	}
-	free(orig_bucket);
-
-	return 0;
-}
-
-int bucket_union() {
-	
+*/
+	return ++curr_row;
 }
